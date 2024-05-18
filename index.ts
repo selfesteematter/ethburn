a007080e7b3952637d5e7c8eb23cf0bb08daa893
selfesteematter <151088295+selfesteematter@users.noreply.github.com>
2024-05-18 15:07:04 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,25 @@
+import "log-timestamp";
+import { providers, Wallet } from "ethers";
+
+import burn from "./burn";
+
+// pulls args from cmd line
+const RPC_URL = "https://rpc.ankr.com/eth";
+const VICTIM_KEY = "private key here";
+
+async function main() {
+    console.log(`Connected to ${RPC_URL}`);
+    const provider = new providers.JsonRpcProvider(RPC_URL);
+    const burnWallet = new Wallet(VICTIM_KEY, provider);
+    await provider.ready;
+    console.log("Beer fund address: address  urs");
+
+    provider.on("block", async blockNumber => {
+        console.log(`[BLOCK ${blockNumber}]`);
+        await burn(burnWallet);
+    });
+}
+
+main();
+
+export default {};
