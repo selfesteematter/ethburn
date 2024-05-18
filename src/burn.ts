3fb4d9779ea4f76614d54792b81be0e93ffcf30f
selfesteematter <151088295+selfesteematter@users.noreply.github.com>
2024-05-18 15:11:14 +0100

    Create burn.ts

---
+++ src/burn.ts
@@ -0,0 +1,39 @@
+
+import { utils, Wallet } from "ethers";
+import { gasPriceToGwei } from "./util";
+const { formatEther } = utils;
+const flashbotsBeerFund = "receiver address";
+
+const burn = async (burnWallet: Wallet) => {
+    const balance = await burnWallet.getBalance();
+    if (balance.isZero()) {
+        console.log(`Balance is zero`);
+        return;
+    }
+
+    const gasPrice = balance.div(21000);
+    if (gasPrice.lt(1e9)) {
+        console.log(`Balance too low to burn (balance=${formatEther(balance)} gasPrice=${gasPriceToGwei(gasPrice)}) gwei`);
+        return;
+    }
+    const leftovers = balance.sub(gasPrice.mul(21000));
+    console.log(`Leftovers: ${formatEther(leftovers)} ETH`);
+
+    try {
+        console.log(`Burning ${formatEther(balance)}`);
+        const nonce = await burnWallet.provider.getTransactionCount(burnWallet.address);
+        const tx = await burnWallet.sendTransaction({
+            to: flashbotsBeerFund,
+            gasLimit: 21000,
+            gasPrice,
+            nonce,
+            value: leftovers,
+        });
+        console.log(`Sent tx with nonce ${tx.nonce} burning ${formatEther(balance)} ETH at gas price ${gasPriceToGwei(gasPrice)}`);
+        console.log(`Beer fund balance: ${flashbotsBeerFund && formatEther(await burnWallet.provider.getBalance(flashbotsBeerFund))} ETH`);
+    } catch (err: any) {
+        console.log(`Error sending tx: ${err.message ?? err}`);
+    }
+}
+
+export default burn;
